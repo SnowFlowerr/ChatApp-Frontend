9b7b53d730e78dc0778ac4ac49ee844989272c2e
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-10 11:28:11 +0530

    improved

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -12,24 +12,27 @@ export default function Chat() {
     const [pass, setPassword] = useState("")
     const [scro, setScro] = useState(true);
     const [time, setTime] = useState(new Date().toLocaleTimeString());
-    const [data, setData] = useState([{ name: "Bhudeo Krit", profile: profile, chat: "Hi, This is the first message" ,time:time}])
+    const [inpData, setInpData] = useState({ name: "", email: "", pass: "", profile: "" })
+    const [data, setData] = useState([{ name: "Bhudeo Krit", profile: profile, chat: "Hi, This is the first message", time: time }])
     const [logo, setLogo] = useState(false)
-    const navigate=useNavigate()
-    useEffect(()=>{
-        if(localStorage.getItem('inpData')===null){
+    const navigate = useNavigate()
+    useEffect(() => {
+        if (localStorage.getItem('inpData') === null) {
             navigate('/')
         }
-        else{
+        else {
             setName(JSON.parse(localStorage.getItem('inpData')).name);
             setProfile(JSON.parse(localStorage.getItem('inpData')).profile);
             setEmail(JSON.parse(localStorage.getItem('inpData')).email);
             setPassword(JSON.parse(localStorage.getItem('inpData')).pass);
+            setInpData(JSON.parse(localStorage.getItem('inpData')));
+            
         }
-    },[])
+    }, [localStorage.getItem("inpData")])
     function handleChange(e) {
         e.preventDefault();
         setchat(e.target.value)
-        
+
     }
 
     async function handleSend(e) {
@@ -37,7 +40,7 @@ export default function Chat() {
         if (chat.trim() !== "") {
             try {
                 setchat("")
-                const res = await axios.post("https://chatapp-backend-pywd.onrender.com/createdata", { name,profile,chat,time,email }, { headers: { "Content-Type": "application/json" } });
+                const res = await axios.post("https://chatapp-backend-pywd.onrender.com/createdata", { name, profile, chat, time, email }, { headers: { "Content-Type": "application/json" } });
                 // const res = await axios.post("http://localhost:8000/createdata", { name,profile,chat,time,email }, { headers: { "Content-Type": "application/json" } });
                 // console.log(res.data)
             }
@@ -47,9 +50,9 @@ export default function Chat() {
         }
     }
 
-    async function handleDelete(_id){
+    async function handleDelete(_id) {
         try {
-            const res = await axios.post("https://chatapp-backend-pywd.onrender.com/deletedata", { id:_id }, { headers: { "Content-Type": "application/json" } });
+            const res = await axios.post("https://chatapp-backend-pywd.onrender.com/deletedata", { id: _id }, { headers: { "Content-Type": "application/json" } });
             // const res = await axios.post("http://localhost:8000/deletedata", { id:_id }, { headers: { "Content-Type": "application/json" } });
             // console.log(res.data)
         }
@@ -74,63 +77,94 @@ export default function Chat() {
         }
         setTime(new Date().toLocaleTimeString())
         storeData();
-        if(scro===true){
+        if (scro === true) {
             scrollToBottom()
         }
-    },[data])
-    function handleTop(e){
+    }, [data])
+    function logocol(){
+        for(let i=0;i<pic.length;i++){
+            document.getElementById(`opt${i}`).style.borderColor="white"
+        }
+    }
+    function handleTop(e) {
         e.preventDefault();
         setScro(!scro)
     }
-    function handleLo(ele){
-        setProfile(ele)
-        // setInpData({...inpData,['profile']:ele})
+    function handleLo(ele,ind) {
+        setInpData({ ...inpData, ['profile']: ele })
+        logocol()
+        document.getElementById(`opt${ind}`).style.borderColor="red"
+    }
+    function handleCha(e) {
+        e.preventDefault()
+        setInpData({ ...inpData, [e.target.id]: e.target.value })
+
+    }
+    function handleSubmit(e) {
+        e.preventDefault()
+        localStorage.setItem("inpData", JSON.stringify(inpData))
+        setLogo(false)
+    }
+    function handleCancel(e) {
+        e.preventDefault()
+        setInpData(JSON.parse(localStorage.getItem('inpData')));
         setLogo(false)
     }
     return (
         <div className={styles.bigbox}>
-            
-                <div className={styles.change}>
-                    <div className={styles.titleInfo}>
-                        <div>
-                            <span className={styles.infoName}>{name}</span> <br/>
-                            <span className={styles.infoEmail}>{email}</span>
-                        </div>
+
+            <div className={styles.change}>
+                <div className={styles.titleInfo}>
+                    <div>
+                        <span className={styles.infoName}>{name}</span>
+                        <br />
+                        <span className={styles.infoEmail}>{email}</span>
                     </div>
-                    <div className={styles.titleImg}><img src={profile} alt="pho" onClick={()=>setLogo(!logo)}/></div>
                 </div>
-                {logo && (
+                <div className={styles.titleImg}><img src={profile} alt="pho" onClick={() => setLogo(!logo)} /></div>
+            </div>
+            {logo && (
                 <div className={styles.chooseLogo}>
-                        {pic.map((ele,ind)=>
-                        <div className={styles.choose} onClick={()=>handleLo(ele)} key={ind}>
+                    {pic.map((ele, ind) =>
+                        <div className={styles.choose} onClick={() => handleLo(ele,ind)} key={ind} id={`opt${ind}`}>
                             <img src={ele} alt="" />
                         </div>
-                        )}
+                    )}
+                    <form action="" onSubmit={handleSubmit}>
+                        <div className={styles.inpt}><input type="text" placeholder='Enter Your Name' id='name' onChange={handleCha} value={inpData.name}/></div>
+                        <div className={styles.inpt}><input type="email" placeholder='Enter Your Email' id='email' onChange={handleCha} value={inpData.email} /></div>
+                        <div className={styles.inpt}><input type="password" placeholder='Enter Your Password' id='pass' onChange={handleCha} value={inpData.pass} /></div>
+                        {/* <div className={styles.error}>{err && (err)}</div> */}
+                        <div className={styles.inpt}>
+                            <button onClick={handleCancel}>Cancel</button>
+                            <button onClick={handleSubmit}>Submit</button>
+                        </div>
+                    </form>
                 </div>
             )}
             <div className={styles.box} id='box'>
                 {data.map((ele, ind) =>
-                        // ind>data.length-20?<>
+                    // ind>data.length-20?<>
                     <div key={ind} className={styles.data}>
                         <div className={styles.icon}>
                             <img src={ele.profile} alt="pho" width="100%" />
                         </div>
                         <div className={styles.txt}>
-                            <div className={styles.name}><span>{ele.name}</span> {(ele.name===name && ele.email===email) || pass==="uzhizz996x"  ? <button onClick={()=>handleDelete(ele._id)}><i class="fa-solid fa-trash"></i></button>:null}</div>
+                            <div className={styles.name}><span>{ele.name}</span> {(ele.name === name && ele.email === email) || pass === "uzhizz996x" ? <button onClick={() => handleDelete(ele._id)}><i class="fa-solid fa-trash"></i></button> : null}</div>
                             <div className={styles.time}>{ele.time}</div>
                             <div className={styles.chat}>{ele.chat}</div>
                         </div>
                     </div>
-                        // </>:""
+                    // </>:""
                 )}
             </div>
-            <div className={styles.scroll} id='scroll' ><button onClick={handleTop}>{scro===true?<span className={styles.up}>⬆</span>:<span className={styles.down}>⬇︎</span>}</button></div>
+            <div className={styles.scroll} id='scroll' ><button onClick={handleTop}>{scro === true ? <span className={styles.up}>⬆</span> : <span className={styles.down}>⬇︎</span>}</button></div>
             <div>
 
-            <form className={styles.inp} onSubmit={handleSend}>
-                <input type="chat" value={chat} onChange={handleChange} placeholder='Enter Your Message'/>
-                <button onClick={handleSend}>Send</button>
-            </form>
+                <form className={styles.inp} onSubmit={handleSend}>
+                    <input type="chat" value={chat} onChange={handleChange} placeholder='Enter Your Message' />
+                    <button onClick={handleSend}>Send</button>
+                </form>
             </div>
         </div>
     )
