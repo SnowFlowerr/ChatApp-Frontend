4114a1afe1298f5a3e9e93c5523bb5ea96541707
Bhudeo Krit <Bhudeokrit@gmail.com>
2024-05-10 15:47:01 +0530

    video added

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -3,6 +3,7 @@ import styles from "./Chat.module.css"
 import axios from "axios"
 import { useNavigate } from 'react-router-dom'
 import pic from "../asset/data"
+import ReactPlayer from 'react-player'
 
 export default function Chat() {
     const [chat, setchat] = useState("")
@@ -11,6 +12,8 @@ export default function Chat() {
     const [profile, setProfile] = useState("")
     const [pass, setPassword] = useState("")
     const [scro, setScro] = useState(true);
+    const [video, setVideo] = useState("");
+    const [isvid, setIsvid] = useState(false);
     const [time, setTime] = useState(new Date().toLocaleTimeString());
     const [inpData, setInpData] = useState({ name: "", email: "", pass: "", profile: "" })
     const [data, setData] = useState([{ name: "Bhudeo Krit", profile: profile, chat: "Hi, This is the first message", time: time }])
@@ -37,10 +40,11 @@ export default function Chat() {
 
     async function handleSend(e) {
         e.preventDefault();
-        if (chat.trim() !== "") {
+        if (chat.trim() !== "" || video.trim()!=="") {
             try {
                 setchat("")
-                const res = await axios.post("https://chatapp-backend-pywd.onrender.com/createdata", { name, profile, chat, time, email }, { headers: { "Content-Type": "application/json" } });
+                setVideo("")
+                const res = await axios.post("https://chatapp-backend-pywd.onrender.com/createdata", { name, profile, chat, time, email, video }, { headers: { "Content-Type": "application/json" } });
                 // const res = await axios.post("http://localhost:8000/createdata", { name,profile,chat,time,email }, { headers: { "Content-Type": "application/json" } });
                 // console.log(res.data)
             }
@@ -110,9 +114,22 @@ export default function Chat() {
         setInpData(JSON.parse(localStorage.getItem('inpData')));
         setLogo(false)
     }
+    function changeVideo(e){
+        e.preventDefault()
+        setVideo(e.target.value)
+    }
+    function handleVideo(e){
+        e.preventDefault()
+        setIsvid(!isvid);
+        if(!isvid){
+            document.getElementById('box').style.height="78.4vh"
+        }
+        else{
+            document.getElementById('box').style.height="84.5vh"
+        }
+    }
     return (
         <div className={styles.bigbox}>
-
             <div className={styles.change}>
                 <div className={styles.titleInfo}>
                     <div>
@@ -121,7 +138,9 @@ export default function Chat() {
                         <span className={styles.infoEmail}>{email}</span>
                     </div>
                 </div>
-                <div className={styles.titleImg}><img src={profile} alt="pho" onClick={() => setLogo(!logo)} /></div>
+                <div className={styles.titleImg}>
+                    <img src={profile} alt="pho" onClick={() => setLogo(!logo)} />
+                </div>
             </div>
             {logo && (
                 <div className={styles.chooseLogo}>
@@ -150,8 +169,15 @@ export default function Chat() {
                             <img src={ele.profile} alt="pho" width="100%" />
                         </div>
                         <div className={styles.txt}>
-                            <div className={styles.name}><span>{ele.name}</span> {(ele.name === name && ele.email === email) || pass === "uzhizz996x" ? <button onClick={() => handleDelete(ele._id)}><i class="fa-solid fa-trash"></i></button> : null}</div>
+                            <div className={styles.name}>
+                                <span>{ele.name}</span>
+                                {(ele.name === name && ele.email === email) || pass === "uzhizz996x" ? <button onClick={() => handleDelete(ele._id)}><i class="fa-solid fa-trash"></i></button> : null}
+                            </div>
                             <div className={styles.time}>{ele.time}</div>
+                            {ele.video &&
+                            <div className={styles.video}>
+                                <ReactPlayer url={ele.video} controls={true} className={styles.player} width="95%" height="13rem" />
+                            </div>}
                             <div className={styles.chat}>{ele.chat}</div>
                         </div>
                     </div>
@@ -160,10 +186,15 @@ export default function Chat() {
             </div>
             <div className={styles.scroll} id='scroll' ><button onClick={handleTop}>{scro === true ? <span className={styles.up}>⬆</span> : <span className={styles.down}>⬇︎</span>}</button></div>
             <div>
-
+            
+                {isvid &&
+                    <form className={styles.inp}>
+                        <input type="chat" value={video} onChange={changeVideo} placeholder='Enter Your Video Link' />
+                    </form>}
                 <form className={styles.inp} onSubmit={handleSend}>
                     <input type="chat" value={chat} onChange={handleChange} placeholder='Enter Your Message' />
-                    <button onClick={handleSend}>Send</button>
+                    <button onClick={handleVideo} type='reset'><i class="fa-solid fa-video"></i></button>
+                    <button onClick={handleSend} type='submit'><i class="fa-solid fa-paper-plane"></i></button>
                 </form>
             </div>
         </div>
